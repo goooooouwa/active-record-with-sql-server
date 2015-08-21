b428b97a6f8483d38c806196397daff5c7a2ce06
Greg <greg.xu@activenetwork.com>
2015-08-21 17:23:08 +0800

    add the association model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ActiveRecord::Base
   has_and_belongs_to_many :facebook_pages, join_table: "EventFacebookPages", foreign_key: :evnt_id
-  belongs_to :user
+  has_one :user_event
+  delegate :user, :to => :user_event, :allow_nil => true
 end
--- app/models/user_event.rb
+++ app/models/user_event.rb
@@ -1,2 +1,6 @@
 class UserEvent < ActiveRecord::Base
+  self.table_name = 'UserEvents'
+
+  belongs_to :user
+  belongs_to :event
 end
