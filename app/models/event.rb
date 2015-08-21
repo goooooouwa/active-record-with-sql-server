815685fe45d092492d45cf17a3b2baf73056cb6d
Greg <greg.xu@activenetwork.com>
2015-08-21 17:51:40 +0800

    setup association for event and org

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ActiveRecord::Base
   has_and_belongs_to_many :facebook_pages, join_table: "EventFacebookPages", foreign_key: :evnt_id
   has_one :user_event
+  belongs_to :organization, foreign_key: :org_id
+
   delegate :user, :to => :user_event, :allow_nil => true
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,3 @@
 class Organization < ActiveRecord::Base
+  has_many :events, foreign_key: :org_id
 end
